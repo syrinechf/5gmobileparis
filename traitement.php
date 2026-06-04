f6ba6f4310cb1f62f6e26e7d6847ab9d2bda427a
syrinechf <syrineechrff@gmail.com>
2026-06-04 17:40:24 +0200

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,10 +1,8 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "repair_shop");
-
+$conn = new mysqli("localhost", "TON_USER", "TON_MOT_DE_PASSE", "TON_NOM_BDD");
 if ($conn->connect_error) {
     die("Erreur connexion");
 }
-
 $nom = $_POST['nom'] ?? '';
 $telephone = $_POST['telephone'] ?? '';
 $email = $_POST['email'] ?? '';
@@ -14,11 +12,9 @@ $type = $_POST['type_reparation'] ?? 'contact';
 $stmt = $conn->prepare("INSERT INTO demandes (nom, telephone, email, message, type_reparation, date_demande) VALUES (?, ?, ?, ?, ?, NOW())");
 $stmt->bind_param("sssss", $nom, $telephone, $email, $message, $type);
 $stmt->execute();
-
 $stmt->close();
 $conn->close();
 
-// redirection (tu peux changer la page)
 header("Location: index.html");
 exit();
 ?>
