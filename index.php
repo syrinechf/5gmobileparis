7428f76b3c7c14171a1d3dbcf4fa1a88a043092e
syrinechf <syrineechrff@gmail.com>
2026-06-09 05:59:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "u530169248_usr_3dET03ua", "Syrinejasim94@.", "u530169248_db_3dET03ua");
+$conn = new mysqli("localhost", "TON_USER", "TON_MOT_DE_PASSE", "TON_NOM_BDD");
 if ($conn->connect_error) { die("Connexion échouée"); }
 
 $success = false;
